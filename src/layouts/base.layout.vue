7de6a85aabbc93e7f090d9b276f0cb190fea9562
yd.b40 <508818+b40yd@users.noreply.github.com>
2025-06-24 11:05:51 +0800

    Update base.layout.vue

--- src/layouts/base.layout.vue
+++ src/layouts/base.layout.vue
@@ -90,6 +90,18 @@ const tools = computed<ToolCategory[]>(() => [
     </template>
 
     <template #content>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1972061408650824"
+     crossorigin="anonymous"></script>
+<!-- tools -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1972061408650824"
+     data-ad-slot="1518578688"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
       <div flex items-center justify-center gap-2>
         <c-button
           circle
